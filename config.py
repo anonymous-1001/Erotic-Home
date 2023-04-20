7e6faad24e0849701d489927f8d41ecacc5286b7
Anonymous <87971370+anonymous-1001@users.noreply.github.com>
2023-04-20 20:02:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#Your bot token, enter token between " token "
+Token = " "
+#add your chat id
+admin_id = 
+#Your group id
+group_id =
+#group name, enter token between " token "
+# this name will not automatically add in start message. you have to enter your group name in it automatically
+group_name = "
+#start message, must enter your start message between " message "
+start_message = " "
